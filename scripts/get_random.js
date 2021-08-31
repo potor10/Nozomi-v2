0e9af9a6fa8e22790ae35ad8d5e11ab023afb8aa
Potor <liupotor4@gmail.com>
2021-08-31 10:11:00 -0700

    added basic character tables

--- scripts/get_random.js
+++ scripts/get_random.js
@@ -1,4 +1,17 @@
-const MasterDatabase = require('../lib/master_database')
+const AccountDatabase = require('../lib/account_database')
+const db = new AccountDatabase()
 
-const master_db = new MasterDatabase()
-console.log(master_db.getRandomUnit(3))
+db.reset()
+
+const id = 'abcacb'
+db.addAccount(id)
+db.addJewels(id, 1500)
+
+db.daily(id)
+
+const Gacha = require('../lib/gacha')
+
+const gacha = new Gacha()
+console.log(gacha.rollTen(id))
+console.log(gacha.rollTen(id))
+console.log(gacha.rollTen(id))
