5da997b8820c9a1696c7cf0395e8c886dbac7863
Potor <liupotor4@gmail.com>
2021-08-31 11:38:38 -0700

    enabled equipment equipping

--- scripts/get_random.js
+++ scripts/get_random.js
@@ -1,7 +1,11 @@
 const AccountDatabase = require('../lib/account_database')
 const db = new AccountDatabase()
 
+const CollectionDatabase = require('../lib/collection_database')
+const db2 = new CollectionDatabase()
+
 db.reset()
+db2.reset()
 
 const id = 'abcacb'
 db.addAccount(id)
@@ -13,5 +17,8 @@ const Gacha = require('../lib/gacha')
 
 const gacha = new Gacha()
 console.log(gacha.rollTen(id))
-console.log(gacha.rollTen(id))
-console.log(gacha.rollTen(id))
+
+let result = gacha.rollTen(id)
+console.log(result)
+
+db2.addEquipment(id, result[0].id, 1)
